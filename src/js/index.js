b958584a7ed27eb15ee6ee05f24891fce641e71b
Ivan-ISS <vanya5@inbox.ru>
2024-01-27 14:00:20 +0500

    project_book-store: added fonts, header, banner section, slider module and styles for different screens

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,23 @@
-import { entities } from './slider';
+import { circleSwitch } from './slider';
 import '../styles/styles.scss';
 
 document.write('Hello, webpack-dev-server');
 
-document.write(entities[1]);
+
+const entities = [
+    {
+        img: './images/png/banner-sale.png',
+    },
+    {
+        img: './images/png/banner-entrepreneurs.png',
+    },
+    {
+        img: './images/png/banner-selection-books.png',
+    }
+];
+
+const picture = document.querySelector('.banner__image-slide');
+const btnCircle = document.querySelectorAll('.selection__circle-btn');
+const cssBtnCircle = 'selection__circle-btn-focus';
+
+circleSwitch(btnCircle, cssBtnCircle, picture, entities);
